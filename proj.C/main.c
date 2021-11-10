b905b9fc55cffc99fae75a4a7ed88bfaf833710b
BENJY33X <abdostaili1999@gmail.com>
2021-11-10 15:57:58 +0100

    la dernier changement

--- proj.C/main.c
+++ proj.C/main.c
@@ -2,28 +2,38 @@
 #include <stdlib.h>
 int main()
 {
-char matiere[30][5] ;
-float notte [6];
+char matiere[30][50] ;
+int notte [15];
 int i=0;
 while(i<5){
 printf("taper la matiere %d\n",i+1);
 scanf("%s",matiere[i]);
+printf("*************************\n");
 i++;
 }
 int c=0;
+int S;
+float M;
 while(c<5){
 printf("taper la notte de %s \n",matiere[c]);
-scanf("%f",&notte[c]);
+scanf("%d",&notte[c]);
+S=S+notte[c];
 c++;
 }
+system("cls");
+M=S/5;
 int m=0;
 while(m<5){
-    printf("la notte de %s est %.2f\n",matiere[m],notte[m]);
+    printf("la notte de %s est %d\n",matiere[m],notte[m]);
     m++;
 }
+printf("*************************\n");
+printf("la somme des notte est %d\n",S);
+printf("*************************\n");
+printf ("la moyenne des votre notes est %.2f",M);
+printf("*************************\n");
+
+
+
     return 0;
 }
-
-
-
-
